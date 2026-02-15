25936b5c0eecae23a472c7415735745a1a3dd469
Swayam <98339243+sswayam23@users.noreply.github.com>
2026-02-16 01:15:21 +0530

    Refactoring the flow of the app
    Refactored the flow of the app. Made it a little more intuitive. 
    Nowhere close to being a finished product but makes things much easier.
    Introduced the concept of remembering tasks and being able to perform multiple operations in the same instance.

--- poc.py
+++ poc.py
@@ -60,11 +60,39 @@ def user_choice_action(user_input):
         for i in task_list:
             print(f'  {i[0]:<12}  |  {i[1]:^20}  | {i[2]:<10}  ')
 
-
+# This is a temporary line, we will fix this. I think this is not deserving of a line in the main window.
 task_list=[['Task Number', 'Task Name','Status']]
 
-x=get_user_choice()
-print(x)
-user_choice_action(x)
-print('finished the action')
+task_list_touched=0
+
+if task_list_touched==0:
+    print("Let us add some tasks to track for today!")
+    user_choice_action(1)
+    task_list_touched=1
+
+# Now that we have the first task added, let us display it and not just close the application. 
+# This will help in at least maintaining the temporary memory. 
+
+#Again, only using this to maintain memory and keep the app open, once we implement the database procedure, we do not need the user to
+#keep the app open to retain memory.
+
+#Another thing I'd like to change is that I feel like there's excessive text. Too much spoonfeeding going on, will fix it after POC is 
+#deemed to be enough.
+exit_app=1
+
+while exit_app:
+    # Did not use the function because I did not want the text to show up.
+    for i in task_list:
+            print(f'  {i[0]:<12}  |  {i[1]:^20}  | {i[2]:<10}  ')
+    user_input=input("To see the menu or to make the changes please press Enter. \n" \
+                 "To quit the app please enter q.")
+
+    if user_input=="":
+        user_choice=get_user_choice()
+        user_choice_action(user_choice)
+    
+    if user_input=='q':
+        exit_app=0
+
+print(" Thank you for using the app.")
 
